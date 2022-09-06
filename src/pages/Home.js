b2bde4a076983fd191301d54fab8f1b7c2682218
EgeKaanGurkan <ege.gurkan2608@gmail.com>
2022-09-06 21:39:20 +0300

    latest changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,11 @@ import "../App.css"
 import {Box} from "@mui/material";
 import {defaultCursorStyle, hoverCursorStyle} from "../redux/actions/cursorStyle";
 import {useDispatch} from "react-redux";
+import {Canvas, useFrame, useLoader} from "@react-three/fiber";
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+import {Model} from "../static/3d/Scene";
+import {Suspense} from "react";
+
 
 export default function Home() {
 
@@ -9,11 +14,19 @@ export default function Home() {
 
   return (
     <Box className={'home'}>
-      <p onMouseEnter={() => dispatch(hoverCursorStyle())} onMouseLeave={() => dispatch(defaultCursorStyle())} className={'homeBannerQuoteText'}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit. Pellentesque sit amet sapien fringilla,
-        mattis ligula consectetur, ultrices mauris.
-      </p>
+      {/*<Canvas className={'homeHeroCanvas'}>*/}
+      {/*  <Suspense fallback={null}>*/}
+      {/*    <Model/>*/}
+      {/*  </Suspense>*/}
+      {/*</Canvas>*/}
+      <div className={'homeBannerQuoteTextContainer'}>
+        <p onMouseEnter={() => dispatch(hoverCursorStyle())} onMouseLeave={() => dispatch(defaultCursorStyle())}
+           className={'homeBannerQuoteText'}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit. Pellentesque sit amet sapien fringilla,
+          mattis ligula consectetur, ultrices mauris.
+        </p>
+      </div>
     </Box>
   )
 }
